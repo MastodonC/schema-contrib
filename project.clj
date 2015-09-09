b306719d38b6b5f30770c9af02e1d36ac85c8a43
Neale Swinnerton <neale@sw1nn.com>
2015-09-09 09:35:09 +0100

    Update project group-id and dependency versions.

--- project.clj
+++ project.clj
@@ -1,10 +1,12 @@
-(defproject schema-contrib "0.1.5"
+(defproject kixi/schema-contrib "0.2.0"
   :description "Additional validators for Prismatic's Schema."
   :url "https://github.com/sfx/schema-contrib"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[instaparse "1.3.0"]
-                 [org.clojure/clojure "1.6.0"]
-                 [org.clojure/test.check "0.5.7"]
-                 [prismatic/schema "0.2.6"]]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/test.check "0.8.1"]
+
+                 [prismatic/schema "1.0.1"]
+                 [instaparse "1.4.1"]]
+
   :resource-paths ["resources"])
